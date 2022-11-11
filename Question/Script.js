a2443c3aa16ac128cc3ec8e41ea7bba834b6591b
Adejane Moreira <adjanead1@gmail.com>
2022-11-10 23:52:45 -0300

    Add files via upload

---
+++ Question/Script.js
@@ -0,0 +1,33 @@
+//Dado um array de inteiros na entrada, calcule o quadrado de cada elemento e salve em um novo array
+const array = [2,3,4,5];
+const arrSub = array.map((num) => Math.pow(num,2));
+console.log(arrSub);
+//A partir de um array informado, mostre a soma de todos os elementos positivos
+let arr = [1, 2, 3, 4, 5, 6];
+let result = arr.reduce((acumulador, valorAtual) => {
+if(valorAtual > 0) {
+return acumulador + valorAtual
+} else {
+return acumulador
+}
+});
+console.log(result)
+//Ordene o array de inteiro informado em ordem decrescente.
+
+//Calcule a média dos elementos de um array de inteiro.
+const arr = [1, 2, 3, 4, 5];
+const average = arr.reduce((a, b) => a + b, 0) / arr.length;
+console.log(average);
+
+//Gerador de iniciais: a partir do nome completo do usuário, mostre as iniciais do seu nome.
+var nome = 'Adejane';
+for (var i = 0; i < nome.length; i++) {
+console.log(nome.charAt(i));
+}
+//[Extra] Dado um array de 2 dimensões, conte e mostre quantos elementos são distintos.
+
+
+
+
+
+
